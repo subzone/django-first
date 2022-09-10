0d79c0ee6c84f2c7d98a6138bd952b272ee84abf
subzone <subzone@live.com>
2022-09-10 13:09:29 +0200

    dfgf

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-umyw(bnr4xs=kvs_oky9^i3r1f#!hlpi1wk2lpjka$xiyg=egu'
+SECRET_KEY = 'django-insecure-umyw(bnr4xs=kvs_oky9^i3r1f#!hlpi1wk2lpjka$xiyg=egu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
